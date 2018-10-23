9e95adc8c42cc75c6c5823a44d2c51011b51569f
Kalif ardy noerwicaksono <kalifardy56@gmail.com>
2018-10-23 13:23:27 +0700

    Update ListViewActivity.java

--- app/src/main/java/com/deaenita/gridviewdanlistview/ListViewActivity.java
+++ app/src/main/java/com/deaenita/gridviewdanlistview/ListViewActivity.java
@@ -9,6 +9,8 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 public class ListViewActivity extends AppCompatActivity {
+    
+   
     ListView lv;
     String[] charactersDC ={"dea","apin","arif","aza","yanuar"};
 
@@ -16,11 +18,11 @@ public class ListViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
-
+// deklarasi listview
         lv =(ListView)findViewById(R.id.lv_namaorang);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,charactersDC);
         lv.setAdapter(adapter);
-
+// list view ketika di click
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
